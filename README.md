47526370f85d50266a8ec3bc390fbd00125241ca
Kristof <kristof@aut.bme.hu>
2023-08-23 13:10:38 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+﻿# Linq to XML lekérdezés labor (kódnév: Linq2XmlSvgLab)
+
+Az előzetes felkészüléshez az alábbi anyagokat tekintsd át:
+
+- Videó a Moodle alatt.
+- Linq dokumentáció: https://docs.microsoft.com/en-us/dotnet/csharp/linq/
+- A code review labor feladathoz: .NET C# coding guides (MS), lásd a leírásban.
+
+Ennek a labornak a célja a Linq to XML lekérdezések gyakorlása. Ehhez első körben kell egy adathalmaz, amire
+utána a lekérdezésekkel megválaszolandó feladatok vonatkoznak. A szemléletesség kedvéért kihasználjuk, hogy az
+SVG vektorgrafikus képformátum egy XML fájl, így a lekérdezések színes téglalapok és feliratok tulajdonságaira
+fognak vonatkozni.
+
+## A fájlok
+
+- README.md: ez a leírás
+- Solutions.cs: ebbe kell elkészíteni a megoldásokat. A feladatok valójában itteni metódusok, így ezek
+fejlécéből és az előtte álló kommentárokból derül ki, mik is a tényleges feladatok.
+- TaskTests.cs: a megoldásokat ellenőrző unit tesztek. Nevük megegyezik a tesztelt metódus nevével.
+- ExtensionMethods.cs: bizonyos funkciók sokkal kényelmesebben használhatók, ha extension methodként
+írjuk meg őket, amihez egy statikus osztály kell. Amit így szeretnél elkészíteni, annak itt van az előre
+előkészített helye.
+- rectangles1.svg, rectangles2.svg: XML alapú, vektorgrafikus kép, amire a unit tesztek az elkészült megoldás helyességét
+tesztelik.
+- rectangles1.png, rectangles2.png: PNG képként a megfelelő SVG fájlok tartalma, hátha van, akinek így könnyebb megnézni.
+
+## A labor elvégzésének lépései
+
+(A labor elején, már most hozz létre egy új branchet és utána azon dolgozz, hogy a pull requestet könnyű legyen majd a labor végén létrehozni! És ha az egyetemen kezdted el a munkát, mielőtt elmész, ne felejtsd el felpusholni a változásokat a laborgépről a githubra!)
+
+- Nézd meg a projektben lévő SVG fájlok és a mellékelt PNG megfelelőik tartalmát. Milyen attribútumok
+tartalmazzák például egy téglalap szélességét és a kontúr vonalvastagságát? Az id, x, y, width, height és style
+attribútumokra szükség lesz a feladatok megoldása során.
+- Nézd meg a példa Linq to XML lekérdezéseket a
+https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/basic-queries-linq-to-xml
+oldalon.
+- A labor feladatainak megoldását a Solutions.cs osztály metódusaiban készítsd el. A feladatok rövid leírása
+is ezen metódusok előtt szerepel a forráskódban. A metódusok helyes működését a
+TasksToComplete unit tesztjei ellenőrzik. Természetesen a kód duplikáció elkerülése érdekében a Solutions
+osztályban tetszőleges további segéd metódusokat is létrehozhatsz. Ugyanazt a szűrést ne írd le kérszer!
+(Ajánlásként kommentárba felsoroltam pár metódust, amit jó eséllyel megéri elkészíteni. Ha van kedved,
+szorgalmi feladatként ezek elkészítése előtt kifejezetten ezeket ellenőrző, további unit teszteket is
+készíthetsz.)
+- Mivel most Linq alapú lekérdezéseket kell majd írni, a ciklusok (while, for, foreach) mind code smellnek
+minősülnek: valószínűleg arra utalak, hogy a megoldásod nem teljesen "Linq-es", ezért amikor csak lehet, került
+el őket.
+- A Linq-es kifejezések egy csomó lépést egymás után hajtanak végre, amit debuggolni elég nehéz. Debug
+célokra ideiglenesen nyugodtan vezess be változókat, de a debugger az IEnumerable értékeit is össze tudja neked gyűjteni, csak húzd rá az egeret a változóra.
+(Az IEnumerable végére ha lehet, ne tegyél .ToArray() hívást még ideiglenesen se, mert úgy marad és később igen komoly
+teljesítmény gondokat, rengeteg másolgatást okozhat. Adatbázis kapcsolat esetén pedig még több gond lehet vele.)
+- A fejlesztés során amint valami helyesen működik, a megfelelő unit teszt zölddé válik. Érdemes bekapcsolni
+a Live Unit Testinget, mivel akkor nem kell mindig kézzel futtatgatni a unit teszteket.
+- A munka során törekedj a kód duplikáció elkerülésére, ami azt is jelentheti, hogy egy korábban már elkészített 
+feladatot kis mértékben módosítani kell, hogy például egy másikkal közös metódust használjon. A cél a végleges
+kód duplikáció mentessége, vagyis néha vissza kell emiatt térni korábbi feladatokhoz és refactorálni kell
+őket. Ez ipari környezetben is így van.
+
+## Tippek, trükkök, kiegészítések a laborhoz a korábbi tapasztalatok alapján
+
+- Annak eldöntésére, hogy egy szöveg egy téglalapban van-e, érdemes egy IsInside segédmetódust készíteni, ami a text x és y pozíciójára (a szöveg bal alsó sarka az SVG formátumnál) eldönti, hogy az a téglalapon belül van-e.
+- Ezen a laboron egyre inkább előjön, hogy konzolra kiírásokkal és erős ránézéssel debuggolni egyre nehezebb. Használjátok a töréspontokat és a debuggert, mert nélküle olyan, mintha főzni akarnátok fakanál nélkül. Lehet, de nem érdemes.
+- Az extension method lényege, hogy nem csak pl. "ExtensionMethods.GetHeight(rect)"-ként lehet meghívni, hanem "rect.GetHeight()"-ként is! Ettől lesz extension method.
+- Ha valahol a double.Parse metódust használod, 2. paraméterként add meg a "CultureInfo.InvariantCulture"-t.
+Így a Windows beállításoktól függetlenül a "." lesz a tizedespont. Magyar esetén "," lenne, ami nagyon bekavarhat.
+- Ha a "rect" változó már egy konkrét téglalapra hivatkozik, akkor úgy tudom ellenőrizni, hogy a vonalvastagsága "width"-e, hogy rect.Attribute("style").Value.Contains($"stroke-width:{width}"). Először lekérem a style nevű attribútumot (érdemes belenézni az SVG fájlba, hogy minek milyen attribútumai vannak és abban milyen értékek lehetnek), aztán ennek az értéke kell, amire megvizsgáljuk, hogy szerepel-e benne egy string, amit pedig a $ kezdetű string interpolationnal írunk le, ahol behelyettesítődik a width értéke.
+
+## A labor végére kiegészítő feladatok
+
+Ha maradt még időd, nézd meg a GroupBy Linq metódus dokumentációját és készíts egy feladatot (unit teszttel) és
+hozzá megoldást a GroupBy használatával.
---
+++ TaskTests.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Linq2XmlSvgLab
+{
+    public class TaskTests
+    {
+        private readonly Solutions s1;
+        private readonly Solutions s2;
+
+        public TaskTests()
+        {
+            s1 = new Solutions("rectangles1.svg");
+            s2 = new Solutions("rectangles2.svg");
+        }
+
+        [Fact]
+        public void GetAllRectangles()
+        {
+            Assert.Equal(7, s1.GetAllRectangles().Count());
+            Assert.Equal(12, s2.GetAllRectangles().Count());
+        }
+
+        [Fact]
+        public void CountTextsWithValue()
+        {
+            Assert.Equal(1, s1.CountTextsWithValue("Négyzetek"));
+            Assert.Equal(3, s1.CountTextsWithValue("Alma"));
+            Assert.Equal(1, s1.CountTextsWithValue("Barack"));
+            Assert.Equal(1, s1.CountTextsWithValue("Körte"));
+
+            Assert.Equal(1, s2.CountTextsWithValue("Gyümölcsök"));
+            Assert.Equal(3, s2.CountTextsWithValue("Szilva"));
+            Assert.Equal(3, s2.CountTextsWithValue("Barack"));
+            Assert.Equal(1, s2.CountTextsWithValue("Banán"));
+            Assert.Equal(2, s2.CountTextsWithValue("Meggy"));
+        }
+
+        [Fact]
+        public void GetRectanglesWithStrokeWidth()
+        {
+            Assert.Equal(2, s1.GetRectanglesWithStrokeWidth(1).Count());
+            Assert.Equal(5, s1.GetRectanglesWithStrokeWidth(2).Count());
+
+            Assert.Equal(8, s2.GetRectanglesWithStrokeWidth(1).Count());
+            Assert.Equal(4, s2.GetRectanglesWithStrokeWidth(2).Count());
+        }
+
+        [Fact]
+        public void GetColorOfRectanglesWithGivenX()
+        {
+            var correctColors = new string[] { "#ff0000", "#0000ff", "#ffffff" };
+            var colors = s1.GetColorOfRectanglesWithGivenX(30);
+            Assert.True(UnorderedCompareSequences(correctColors, colors));
+
+            correctColors = new string[] { "#ffffff", "#ff0000", "#ff0000", "#0000ff" };
+            colors = s2.GetColorOfRectanglesWithGivenX(20);
+            Assert.True(UnorderedCompareSequences(correctColors, colors));
+        }
+
+        [Fact]
+        public void GetRectangleLocationById()
+        {
+            AssertLocation(80.0, 180.0, s1.GetRectangleLocationById("rectTeal"));
+            AssertLocation(20.0, 33.75, s2.GetRectangleLocationById("rectWhite"));
+        }
+
+        private void AssertLocation(double correctX, double correctY, (double X, double Y) result)
+        {
+            Assert.Equal(correctX, result.X, 3);
+            Assert.Equal(correctY, result.Y, 3);
+        }
+
+        [Fact]
+        public void GetIdOfRectangeWithLargestY()
+        {
+            Assert.Equal("rectTeal", s1.GetIdOfRectangeWithLargestY());
+            Assert.Equal("rectLowerYellow", s2.GetIdOfRectangeWithLargestY());
+        }
+
+        [Fact]
+        public void GetRectanglesAtLeastTwiceAsHighAsWide()
+        {
+            var ids = s1.GetRectanglesAtLeastTwiceAsHighAsWide().ToArray();
+            Assert.Equal(2, ids.Length);
+            Assert.Contains("rectBlue", ids);
+            Assert.Contains("rectGreen", ids);
+
+            ids = s2.GetRectanglesAtLeastTwiceAsHighAsWide().ToArray();
+            Assert.Single(ids);
+            Assert.Equal("rectLowerBlue", ids[0]);
+        }
+
+        [Fact]
+        public void GetColorsOfRectsInGroup()
+        {
+            var colors = s1.GetColorsOfRectsInGroup("group1");
+            Assert.True(UnorderedCompareSequences<string>(new string[] { "#ff0000", "#ffff00" },
+                colors));
+
+            colors = s2.GetColorsOfRectsInGroup("group2");
+            Assert.True(UnorderedCompareSequences<string>(new string[] { "#00ff00", "#0000ff" },
+                colors));
+        }
+
+        [Fact]
+        public void GetRectanglesWithTextInside()
+        {
+            Assert.Equal(3, s1.GetRectanglesWithTextInside().Count());
+            Assert.Equal(8, s2.GetRectanglesWithTextInside().Count());
+        }
+
+        [Fact]
+        public void GetSingleTextInSingleRectangleWithColor()
+        {
+            Assert.Equal("Barack", s1.GetSingleTextInSingleRectangleWithColor("#ff00ff"));
+            Assert.Null(s1.GetSingleTextInSingleRectangleWithColor("#00ff00"));
+            Assert.Equal("Alma", s1.GetSingleTextInSingleRectangleWithColor("#ffffff"));
+
+            Assert.Equal("Meggy", s2.GetSingleTextInSingleRectangleWithColor("#ff00ff"));
+            Assert.Equal("Szilva", s2.GetSingleTextInSingleRectangleWithColor("#ffffff"));
+        }
+
+        [Fact]
+        public void GetTextsOutsideRectangles()
+        {
+            var correctTexts = new string[] { "Alma", "Körte", "Négyzetek" };
+            Assert.True(UnorderedCompareSequences(correctTexts, s1.GetTextsOutsideRectangles()));
+
+            correctTexts = new string[] { "Banán", "Gyümölcsök" };
+            Assert.True(UnorderedCompareSequences(correctTexts, s2.GetTextsOutsideRectangles()));
+        }
+
+        [Fact]
+        public void GetSingleRectanglePairCloseToEachOther()
+        {
+            AssertIdPair("rectWhite", "rectBlue", s1.GetSingleRectanglePairCloseToEachOther(5.0));
+            AssertIdPair("rectTeal", "rectGreen", s2.GetSingleRectanglePairCloseToEachOther(5.0));
+        }
+
+        private void AssertIdPair(string correctId1, string correctId2, (string id1, string id2) result)
+        {
+            Assert.True((result.id1 == correctId1 && result.id2 == correctId2)
+                || (result.id1 == correctId2 && result.id2 == correctId1));
+        }
+
+        [Fact]
+        public void GetBoundingRectangleColorListForEveryText()
+        {
+            var result = s1.GetBoundingRectangleColorListForEveryText();
+            Assert.True(UnorderedCompareSequences<string>(new string[] { "#ffff00", "#ffffff" }, result["Alma"]));
+            Assert.True(UnorderedCompareSequences<string>(new string[] { "#ff00ff" }, result["Barack"]));
+            Assert.False(result.Contains("Szilva"));
+            Assert.False(result["Körte"].Any());
+
+            result = s2.GetBoundingRectangleColorListForEveryText();
+            Assert.True(UnorderedCompareSequences<string>(new string[] { "#ffffff", "#00ffff", "#ffff00" }, result["Szilva"]));
+            Assert.True(UnorderedCompareSequences<string>(new string[] { "#ff00ff", "#00ff00" }, result["Meggy"]));
+            Assert.False(result.Contains("Alma"));
+            Assert.False(result["Banán"].Any());
+
+        }
+
+        [Fact]
+        public void ConcatenateOrderedTextsInsideRectangles()
+        {
+            string correctResult = "Alma, Alma, Barack";
+            Assert.Equal(correctResult, s1.ConcatenateOrderedTextsInsideRectangles());
+
+            correctResult = "Barack, Barack, Barack, Meggy, Meggy, Szilva, Szilva, Szilva";
+            Assert.Equal(correctResult, s2.ConcatenateOrderedTextsInsideRectangles());
+        }
+
+        [Fact]
+        public void GetEffectiveWidthAndHeightWithGivenStrokeThickness()
+        {
+            AssertWidthHeight(s1, 1, 110.0, 110.0);
+            AssertWidthHeight(s1, 2, 140.0, 190.0);
+            AssertWidthHeight(s2, 1, 170.0, 230.0);
+            AssertWidthHeight(s2, 2, 170.0, 216.25);
+        }
+
+        private void AssertWidthHeight(Solutions s, int strokeThickness, double width, double height)
+        {
+            (double w, double h) = s.GetEffectiveWidthAndHeight(strokeThickness);
+            Assert.Equal(width, w, 3);
+            Assert.Equal(height, h, 3);
+        }
+
+        #region Helpers for the unit tests and their tests
+        [Fact]
+        public void TestUnorderedCompareSequences()
+        {
+            Assert.True(UnorderedCompareSequences(new int[] {1, 2, 3}, new int[] {1, 2, 3}));
+            Assert.True(UnorderedCompareSequences(new int[] {1, 2, 3}, new int[] {1, 3, 2}));
+            Assert.True(UnorderedCompareSequences(new int[] {1, 2, 2}, new int[] {2, 1, 2}));
+            Assert.False(UnorderedCompareSequences(new int[] { 1 }, new int[] { 1, 2 }));
+            Assert.False(UnorderedCompareSequences(new int[] { 1 }, new int[] { 2, 3}));
+        }
+
+        private bool UnorderedCompareSequences<T>(IEnumerable<T> s1, IEnumerable<T> s2)
+        {
+            return s1.OrderBy(i => i).SequenceEqual(s2.OrderBy(j => j));
+        }
+        #endregion
+    }
+}
